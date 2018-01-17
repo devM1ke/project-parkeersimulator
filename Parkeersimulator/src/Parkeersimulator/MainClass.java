eaebac6fb6da084a5178c382685b342bec8e2d82
Mike <mk.dijkstra@st.hanze.nl>
2018-01-17 11:25:36 +0100

    Parkeersimulator + main class

---
+++ Parkeersimulator/src/Parkeersimulator/MainClass.java
@@ -0,0 +1,14 @@
+package Parkeersimulator;
+
+public class MainClass {
+	
+  public MainClass() {
+	  
+  }
+  
+  public static void main(String[] args) {
+    Simulator simulator = new Simulator();
+    simulator.run();
+  }
+  
+ }
