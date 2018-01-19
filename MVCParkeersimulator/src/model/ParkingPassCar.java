0311a2ed15a3deb778cd6ae089a2526db66a7a54
Mike <mk.dijkstra@st.hanze.nl>
2018-01-19 11:56:21 +0100

    added models to MVC

--- MVCParkeersimulator/src/model/ParkingPassCar.java
+++ MVCParkeersimulator/src/model/ParkingPassCar.java
@@ -1,5 +1,19 @@
 package model;
 
-public class ParkingPassCar {
+import java.util.Random;
+import java.awt.*;
 
+public class ParkingPassCar extends Car {
+	private static final Color COLOR=Color.blue;
+	
+    public ParkingPassCar() {
+    	Random random = new Random();
+    	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
+        this.setMinutesLeft(stayMinutes);
+        this.setHasToPay(false);
+    }
+    
+    public Color getColor(){
+    	return COLOR;
+    }
 }
--- MVCParkeersimulator/src/runner/MainClass.java
+++ MVCParkeersimulator/src/runner/MainClass.java
@@ -1,2 +1,17 @@
 package runner;
 
+import Parkeersimulator.Simulator;
+
+public class MainClass {
+	
+	  public MainClass() {
+		  
+	  }
+	  
+	  public static void main(String[] args) {
+	    Simulator simulator = new Simulator();
+	    simulator.run();
+	  }
+	  
+	 }
+
