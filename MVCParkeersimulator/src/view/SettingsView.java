9b23a9be67af122e8089512e89122c77907f195e
Mike <mk.dijkstra@st.hanze.nl>
2018-01-23 20:10:23 +0100

    added settingsview(not finished)

---
+++ MVCParkeersimulator/src/view/SettingsView.java
@@ -0,0 +1,91 @@
+package view;
+
+import javax.swing.*;
+
+import model.Model;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class SettingsView extends AbstractView{
+
+    private JFrame settingsFrame;
+    private JTextField t1, t2;
+
+    public SettingsView(Model model)
+    {
+    	super(model);
+
+    }
+
+    private JPanel CreatePanel()
+    {
+        JPanel content = new JPanel(new GridLayout(3,2));
+
+        JLabel speedlabel = new JLabel("Snelheid");
+        JTextField speedtext = new JTextField(String.valueOf(Model.getTickPause()+"") ,5);
+
+        content.add(speedlabel);
+        content.add(speedtext);
+
+
+        content.add(updateButton());
+        content.add(cancelButton());
+
+        return content;
+    }
+
+    public JButton cancelButton()
+    {
+        JButton cancel = new JButton("Cancel");
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                settingsFrame.dispose();
+            }
+        });
+
+        return cancel;
+    }
+
+    public JButton updateButton()
+    {
+
+        JButton update = new JButton("Update");
+        update.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try
+                {
+
+                    String speed = t2.getText();
+                    Model.setTickPause(Integer.parseInt(speed));
+
+                    AbstractView.updateView();
+                    settingsFrame.dispose();
+                }
+                catch(Exception ex)
+                {
+                    JOptionPane.showMessageDialog(settingsFrame,
+                            "Geen geldig nummer.",
+                            "OPGELET",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
+        return update;
+    }
+
+    public void updateView()
+    {
+        repaint();
+    }
+
+    public void setVisibility(boolean visibility)
+    {
+        setVisible(visibility);
+    }
+}
