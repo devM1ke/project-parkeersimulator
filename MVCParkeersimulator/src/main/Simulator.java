2eb9832c8c7b6dfc010658ccf9290e0fbff86fec
Mike <mk.dijkstra@st.hanze.nl>
2018-01-24 12:47:17 +0100

    deleted bs

--- MVCParkeersimulator/src/main/Simulator.java
+++ MVCParkeersimulator/src/main/Simulator.java
@@ -25,25 +25,22 @@ public class Simulator{
 		screen = new JFrame();
 		carparkview = new CarParkView(model);
 		pieview = new PieView(model);
-<<<<<<< HEAD
+
 		managementview = new ManagementView(model);
-=======
+
 		//menubarview = new MenuBarView(model);
 		//settingsview = new SettingsView(model);
->>>>>>> dfaaf478e9cc3a85d51909cabfbf958ae46a4e33
+
 		
 		screen.getContentPane().add(carparkview);
 		screen.getContentPane().add(pieview);
 		//screen.getContentPane().add(menubarview);
 		//screen.getContentPane().add(settingsview);
-<<<<<<< HEAD
+
 		//screen.getContentPane().add(managementview);		
 
 		screen.setVisible(true);
-=======
-		
-		
->>>>>>> dfaaf478e9cc3a85d51909cabfbf958ae46a4e33
+
 		screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		screen.pack();
 		screen.setSize(1000, 800);
