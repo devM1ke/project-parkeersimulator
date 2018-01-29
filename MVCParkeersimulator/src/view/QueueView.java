56e9482b963cbbe027aee122b42e4ef631be8893
Jelle de Jonge <jhdj1992@hotmail.com>
2018-01-29 16:17:20 +0100

    Added label

--- MVCParkeersimulator/src/view/QueueView.java
+++ MVCParkeersimulator/src/view/QueueView.java
@@ -7,7 +7,7 @@ import javax.swing.JLabel;
 import model.Model;
 
 public class QueueView extends AbstractView {
-private JLabel wachtrijLabel, gewoneIngangLabel, abonneesWachtrij, reserveringWachtrij, betalingsrijLabel, uitgangsLabel;
+private JLabel wachtrijLabel, gewoneIngangLabel, abonneesWachtrij, reserveringWachtrij, betalingsrijLabel, uitgangsLabel, vertrokkenLabel;
 	public QueueView(Model model){
 		super(model);
 		this.model = model;
@@ -17,12 +17,14 @@ private JLabel wachtrijLabel, gewoneIngangLabel, abonneesWachtrij, reserveringWa
 		reserveringWachtrij = new JLabel("Reserveringen in de rij: ");
         betalingsrijLabel = new JLabel("Huidige betalingsrij: ");
         uitgangsLabel = new JLabel("Uitgang wachtrij: ");
+        vertrokkenLabel = new JLabel("Mensen vertrokken uit de rij: ");
 		add(wachtrijLabel);
 		add(gewoneIngangLabel);
 		add(abonneesWachtrij);
 		add(reserveringWachtrij);
 		add(betalingsrijLabel);
 		add(uitgangsLabel);
+		add(vertrokkenLabel);
 	}
 
 	public void paintComponent(Graphics g){
@@ -33,5 +35,6 @@ private JLabel wachtrijLabel, gewoneIngangLabel, abonneesWachtrij, reserveringWa
 		reserveringWachtrij.setText("Reserveringen in de rij: Poep!");
 		betalingsrijLabel.setText("Huidige betalingsrij: " + model.getSizePaymentCarQueue());
 		uitgangsLabel.setText("Uitgang wachtrij: " + model.getSizeExitCarQueue());
+		vertrokkenLabel.setText("Vertrokken uit de rij: Poepie!");
 	}
 }
